b34f7d8cdcf31233544c9229759b76f45c31f713
BharatiChauhan <chauhanbharati9951@gmail.com>
2023-10-19 15:29:09 +0530

    changes in yml file

--- src/main/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplication.java
+++ src/main/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
 @EnableAutoConfiguration
 public class PostgreswithgithubactionApplication {
 
--- src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
+++ src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
@@ -1,16 +1,10 @@
 package com.example.postgreswithgithubaction;
 
 import org.junit.jupiter.api.Test;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
-
-
-
 class PostgreswithgithubactionApplicationTests {
 
 	@Test
