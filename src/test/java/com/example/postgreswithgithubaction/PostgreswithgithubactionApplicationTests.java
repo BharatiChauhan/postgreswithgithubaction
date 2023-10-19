2b80c7687bc0ca432dc99ceb036056763b752974
BharatiChauhan <chauhanbharati9951@gmail.com>
2023-10-19 12:28:19 +0530

    commit

--- src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
+++ src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
@@ -2,11 +2,13 @@ package com.example.postgreswithgithubaction;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@WebMvcTest
 
 
 class PostgreswithgithubactionApplicationTests {
