0d2bb86931c7e688e6eafe39a20f387f121833b1
BharatiChauhan <chauhanbharati9951@gmail.com>
2023-10-19 13:20:35 +0530

    changes in yml file

--- src/main/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplication.java
+++ src/main/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
+
 @EnableAutoConfiguration
 public class PostgreswithgithubactionApplication {
 
