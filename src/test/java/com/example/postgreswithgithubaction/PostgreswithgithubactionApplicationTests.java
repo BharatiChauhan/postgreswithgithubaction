1864d06564ab8221e2486ef4bf585f312e82a095
BharatiChauhan <chauhanbharati9951@gmail.com>
2023-10-19 16:23:02 +0530

    changes in yml file

--- src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
+++ src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.postgreswithgithubaction;
+package test.com.example.postgreswithgithubaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
