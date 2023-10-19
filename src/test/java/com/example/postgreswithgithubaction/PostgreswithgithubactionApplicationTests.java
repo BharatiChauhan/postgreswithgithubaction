37fba89a190bab4edce754084cc020b8694fec56
BharatiChauhan <chauhanbharati9951@gmail.com>
2023-10-19 16:40:57 +0530

    changes

--- src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
+++ src/test/java/com/example/postgreswithgithubaction/PostgreswithgithubactionApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example.postgreswithgithubaction;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest(classes = {PostgreswithgithubactionApplication.class})
+//@SpringBootTest(classes = {PostgreswithgithubactionApplication.class})
+@DataJpaTest
 class PostgreswithgithubactionApplicationTests {
 
 	@Test
